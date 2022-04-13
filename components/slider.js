7c2bfe2e20f5de0f8a4689ac69835c6a50db8f85
yanwar01 <yanwarrama55@gmail.com>
2022-04-13 10:07:15 +0700

    third commit

--- components/slider.js
+++ components/slider.js
@@ -4,10 +4,10 @@ const Slider = () => {
             <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src="/image/1.jpg" className="d-block w-100" alt="..." />
+                        <img src="/image/1.jpg" className="d-block w-60" alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="/image/2.jpg" className="d-block w-100" alt="..." />
+                        <img src="/image/2.jpg" className="d-block w-60" alt="..." />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
